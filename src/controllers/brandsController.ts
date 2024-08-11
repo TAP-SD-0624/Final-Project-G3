1c6e9531d9e1a1ad3f8886e76b59e4d60baa4a32
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-11 10:38:59 +0300

    implement create new brand feature

--- src/controllers/brandsController.ts
+++ src/controllers/brandsController.ts
@@ -1,11 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 import errorHandler from '../utils/errorHandler';
 import Brand from '../models/Brand';
+import APIError from '../utils/APIError';
 
-const createBrand = errorHandler(
+const createNewBrand = errorHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    
+    const brandName = req.body.name;
+    if(!brandName){
+        return next(new APIError('please include the brand name', 400));
+    }
+    await Brand.create({
+        name:brandName
+      });
+      res.status(201).json({
+        message: 'Brand added successfully',
+      });
   }
 );
 
-export { createBrand };
+export { createNewBrand };
--- src/routes/brandRoutes.ts
+++ src/routes/brandRoutes.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express';
+import authMiddleware from '../middlewares/authMiddleware';
+import adminMiddleware from '../middlewares/adminMiddleware';
+import { createNewBrand } from '../controllers/brandsController';
 
 const brandRouter = Router();
+
+brandRouter.route('/createBrand').post(authMiddleware,adminMiddleware, createNewBrand);
 
 export default brandRouter;
