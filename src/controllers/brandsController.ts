2868eccc9bd2e59b788e4380d695a33754eed28a
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-12 14:34:42 +0300

    fix response logic

--- src/controllers/brandsController.ts
+++ src/controllers/brandsController.ts
@@ -5,7 +5,7 @@ import APIError from '../utils/APIError';
 import checkIfBrandExists from '../services/brandService';
 
 const createNewBrand = errorHandler(
-  async(req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const brandName = req.body.name;
     if (await checkIfBrandExists(brandName)) {
       return next(new APIError('Brand already exist', 400));
@@ -19,18 +19,20 @@ const createNewBrand = errorHandler(
   },
 );
 const getAllBrands = errorHandler(
-  async(req: Request, res: Response, next: NextFunction) => {
+  async (req: Request, res: Response, next: NextFunction) => {
     const brands = await Brand.findAll({
       attributes: ['id', 'name'],
     });
-    if (brands.length <= 0){
+    if (brands.length <= 0) {
       res.status(200).send({
         message: 'No brands found',
       });
     }
-    res.status(200).json({
-      brands,
-    });
+    else {
+      res.status(200).json({
+        brands,
+      });
+    }
   },
 );
 
