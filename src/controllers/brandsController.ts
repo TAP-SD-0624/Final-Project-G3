d42c68c981b9ce90652421fd32d58c64c7e4ac8c
Mahmoud M. A. Darabaih <mahmoud.m.darabaih@gmail.com>
2024-08-12 10:56:03 +0300

    fix PR

--- src/controllers/brandsController.ts
+++ src/controllers/brandsController.ts
@@ -23,7 +23,12 @@ const getAllBrands = errorHandler(
     const brands = await Brand.findAll({
       attributes: ['id', 'name'],
     });
-    res.status(201).json({
+    if (brands.length <= 0){
+      res.status(200).send({
+        message: 'No brands found',
+      });
+    }
+    res.status(200).json({
       brands,
     });
   },
