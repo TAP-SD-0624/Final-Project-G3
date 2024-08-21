b7637a8f3f97d39f7f05b1445c6d4d34aa202694
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-08-21 21:33:08 +0300

    Basic setup and testing CLI logging

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/.env' });
 import app from './app';
 import sequelize from './database';
 import associateModels from './models/associations';
+import { logger } from './loggers/logger';
 
 const PORT: number | undefined = Number(process.env.PORT) || 80;
 
@@ -16,12 +17,12 @@ const startServer = async() => {
   try {
     associateModels();
     await sequelize.sync(); // { force: true } for development only to drop and recreate tables
-    console.log('Database & tables created!');
+    logger.info('Database & tables created!');
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      logger.info(`Server running on port ${PORT}`);
     });
   } catch (error) {
-    console.error('Unable to start the server:', error);
+    logger.error('Unable to start the server:', error);
   }
 };
 
