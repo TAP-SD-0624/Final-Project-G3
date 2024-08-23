070182a35b71ac9570bee566a7f9dca7a8260ad1
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-08-23 19:28:40 +0300

    made cors unrestricted

--- src/app.ts
+++ src/app.ts
@@ -24,24 +24,24 @@ app.use(cookieParser()); // For parsing cookies
 
 // setup CORS allowed origins
 
-const allowedOrigins: string[] = [
-  'http://127.0.0.1:4000',
-  'https://backend-final-g3-qngl.onrender.com',
-];
+// const allowedOrigins: string[] = [
+//   'http://127.0.0.1:4000',
+//   'https://backend-final-g3-qngl.onrender.com',
+// ];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    // Allow requests with no origin, like mobile apps or curl requests
-    if (!origin) {
-      return callback(null, true);
-    }
-    if (allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+app.use(cors(
+  // origin: (origin, callback) => {
+  //   // Allow requests with no origin, like mobile apps or curl requests
+  //   if (!origin) {
+  //     return callback(null, true);
+  //   }
+  //   if (allowedOrigins.indexOf(origin) !== -1) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+));
 
 // limit the number of requests sent to the server to under 500 requests per minute.
 app.use(
