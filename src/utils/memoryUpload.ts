e3f23dc14cd8975b0074a4f9b1685f5a898bdd41
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-08-25 19:03:11 +0300

    added constant for image size limit in .env file

--- src/utils/memoryUpload.ts
+++ src/utils/memoryUpload.ts
@@ -2,10 +2,12 @@ import multer, { StorageEngine } from 'multer';
 
 const storage: StorageEngine = multer.memoryStorage();
 
+const IMAGE_SIZE_LIMIT = process.env.IMAGE_SIZE_LIMIT || '2000000';
+
 const memoryUpload = multer({
   storage,
   limits: {
-    fileSize: 2000000, // 2 MB limit for file size.
+    fileSize: parseInt(IMAGE_SIZE_LIMIT), // 2 MB limit for file size.
   },
 });
 
