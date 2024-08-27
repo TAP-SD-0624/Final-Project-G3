85c9b3856f0ded10d1172e4cb7abab7fc9a46ff7
Ahmad Albarasy <ahmadbarasy@gmail.com>
2024-08-28 00:12:50 +0300

    fixed hooked logic

--- src/hooks/addReviewHook.ts
+++ src/hooks/addReviewHook.ts
@@ -1,43 +1,30 @@
 import Review from '../db-files/models/Review';
-import Product from '../db-files/models/Product';
+// import Product from '../db-files/models/Product';
 import { oneProductService } from '../services/productService';
+import APIError from '../utils/APIError';
 
-const recalculateProductRatingHook = async(reviewId: string): Promise<void> => {
-  const review = await Review.findByPk(reviewId, {
-    include: [
-      {
-        model: Product,
-        attributes: ['id'],
-      },
-    ],
-  }) as Review & { productId: string };
-
-  if (!review) {
-    throw new Error('Review not found');
-  }
-
-  // Extract productId from the review
-  const { productId } = review;
-
-  // Fetch the product using the productId
+const recalculateProductRatingHook = async(productId: string): Promise<void> => {
   const product = await oneProductService({
     where: {
       id: productId,
     },
   });
-
   if (!product) {
-    throw new Error('Product not found');
+    throw new APIError('Something wrong happened', 500);
   }
-
   // Get all reviews for the product
   const reviews = await Review.findAll({
     where: { productId: product.id },
   });
-
-  // Calculate average rating
-  const ratings = reviews.map((r) => r.rating);
-  const averageRating = ratings.reduce((acc, current) => acc + current, 0) / ratings.length;
+  // if all reviews on a product got deleted, return to default value: 5
+  let averageRating;
+  if (reviews.length === 0){
+    averageRating = 5;
+  } else {
+    // Calculate average rating
+    const ratings = reviews.map((r) => r.rating);
+    averageRating = ratings.reduce((acc, current) => acc + current, 0) / ratings.length;
+  }
 
   // Update product rating
   product.rating = averageRating;
